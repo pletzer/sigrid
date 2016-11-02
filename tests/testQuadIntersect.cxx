fd41de4f42619ebd628fea50b801fb3a44796861
Alex Pletzer <alexander@gokliya.net>
2016-11-02 16:57:50 +1300

    + partial intersection test

--- tests/testQuadIntersect.cxx
+++ tests/testQuadIntersect.cxx
@@ -104,12 +104,44 @@
  	return true;
  }
 
+  bool testPartial3Points() {
+
+    double quad2Node0[] = {0., 0.};
+    double quad2Node1[] = {1., 0.};
+    double quad2Node2[] = {1., 1.};
+    double quad2Node3[] = {0., 1.};
+
+    double quad1Node0[] = {1.1, 0.4};
+    double quad1Node1[] = {2.0, 0.4};
+    double quad1Node2[] = {1.3, 1.1};
+    double quad1Node3[] = {0.6, 1.1};
+
+    const double* quad1Coords[] = {quad1Node0, quad1Node1, quad1Node2, quad1Node3};
+    const double* quad2Coords[] = {quad2Node0, quad2Node1, quad2Node2, quad2Node3};
+
+    SgQuadIntersect_type* qis = NULL;
+    SgQuadIntersect_new(&qis, quad1Coords, quad2Coords);
+    int numPoints;
+    double* points = NULL;
+    SgQuadIntersect_getPoints(&qis, &numPoints, &points);
+    SgQuadIntersect_del(&qis);
+
+    std::cout << "testPartial3Points: num intersection points = " << numPoints << '\n';
+
+    if (numPoints != 3) {
+        /// error
+        return false;
+    }
+    // OK
+    return true;
+ }
 
 int main(int argc, char** argv) {
 
 	if (!testNoOverlap()) return 1;
 	if (!testQuad2IsInsideQuad1()) return 2;
-	if (!testQuad1IsInsideQuad2()) return 2;
+	if (!testQuad1IsInsideQuad2()) return 3;
+    if (!testPartial3Points()) return 4;
 
 	std::cout << "SUCCESS\n";
 	return 0;
