4bbdfeb685e15767ea6e4dd3224c0eaf44a4e28b
Alexander Pletzer <alexander@gokliya.net>
2016-11-22 14:00:43 +1300

    moved BoxIterator API to C++

--- cpp/SgBoxIterator.cpp
+++ cpp/SgBoxIterator.cpp
@@ -2,56 +2,34 @@
  * Box iterator class
  */
  
- #include "SgNdims.h"
- #include "SgBoxIterator.h"
- #include <iostream>
+#include "SgBoxIterator.h"
 
- extern "C"
- int SgBoxIterator_new(SgBoxIterator_type** self,
+extern "C"
+int SgBoxIterator_new(SgBoxIterator_type** self,
                        const int* begInds, 
                        const int* endInds) {
-
-  *self = new SgBoxIterator_type();
-
-  (*self)->lowEnd.resize(NDIMS_TOPO);
-  (*self)->dims.resize(NDIMS_TOPO);
-  (*self)->dimProd.resize(NDIMS_TOPO);
-  (*self)->numElems = 1;
-  for (size_t i = 0; i < NDIMS_TOPO; ++i) {
-    (*self)->lowEnd[i] = begInds[i];
-    (*self)->dims[i] = endInds[i] - begInds[i];
-    (*self)->dimProd[i] = 1;
-    (*self)->numElems *= (*self)->dims[i];
-  }
-  for (int i = (int) NDIMS_TOPO - 2; i >= 0; --i) {
-    // last index varies fastest
-    (*self)->dimProd[i] =  (*self)->dimProd[i + 1] * (*self)->dims[i + 1];
-  }
+  *self = new SgBoxIterator_type(begInds, endInds);
   return 0;
- }
+}
                        
- extern "C"
- int SgBoxIterator_del(SgBoxIterator_type** self) {
-
+extern "C"
+int SgBoxIterator_del(SgBoxIterator_type** self) {
   delete *self;
   return 0;
- }
+}
  
- extern "C"
- int SgBoxIterator_getNumberOfElements(SgBoxIterator_type** self,
-                                     int* num) {
- 	*num = (*self)->numElems;
+extern "C"
+int SgBoxIterator_getNumberOfElements(SgBoxIterator_type** self,
+                                      int* num) {
+ 	*num = (*self)->getNumberOfElements();
  	return 0;
- }
+}
  
- extern "C"
- int SgBoxIterator_getElement(SgBoxIterator_type** self,
+extern "C"
+int SgBoxIterator_getElement(SgBoxIterator_type** self,
                               int index,
                               int inds[]) {
-  for (size_t i = 0; i < NDIMS_TOPO; ++i) {
-    inds[i] = (*self)->lowEnd[i] + 
-        index / (*self)->dimProd[i] % (*self)->dims[i];
-  }
+  (*self)->getElement(index, inds);
  	return 0;
- }
+}
  
--- cpp/SgBoxIterator.h
+++ cpp/SgBoxIterator.h
@@ -2,34 +2,66 @@
  * Box iterator class
  */
  
- #ifndef SG_BOX_ITERATOR_H
- #define SG_BOX_ITERATOR_H
+#ifndef SG_BOX_ITERATOR_H
+#define SG_BOX_ITERATOR_H
  
- #include <vector>
+#include <vector>
+#include "SgNdims.h"
+#include <iostream>
  
- struct SgBoxIterator_type {
- 	std::vector<int> lowEnd;
- 	std::vector<int> dims;
+struct SgBoxIterator_type {
+    std::vector<int> lowEnd;
+    std::vector<int> dims;
  	std::vector<int> dimProd;
-  int numElems;
- };
+    int numElems;
+
+    SgBoxIterator_type(const int* begInds, const int* endInds) {
+        this->lowEnd.resize(NDIMS_TOPO);
+        this->dims.resize(NDIMS_TOPO);
+        this->dimProd.resize(NDIMS_TOPO);
+        this->numElems = 1;
+        for (size_t i = 0; i < NDIMS_TOPO; ++i) {
+            this->lowEnd[i] = begInds[i];
+            this->dims[i] = endInds[i] - begInds[i];
+            this->dimProd[i] = 1;
+            this->numElems *= this->dims[i];
+        }
+        for (int i = (int) NDIMS_TOPO - 2; i >= 0; --i) {
+            // last index varies fastest
+            this->dimProd[i] =  this->dimProd[i + 1] * this->dims[i + 1];
+        }
+    }
+
+    ~SgBoxIterator_type() {}
+
+    int getNumberOfElements() const {
+        return this->numElems;
+    }
+
+    void getElement(int index, int inds[]) {
+        for (size_t i = 0; i < NDIMS_TOPO; ++i) {
+            inds[i] = this->lowEnd[i] + 
+                index / this->dimProd[i] % this->dims[i];
+        }
+    }
+};
  
 #ifdef __cplusplus
 extern "C" {
 #endif
 
- int SgBoxIterator_new(SgBoxIterator_type** self,
-                       const int* begInds, 
-                       const int* endInds);
+int SgBoxIterator_new(SgBoxIterator_type** self,
+                      const int* begInds, 
+                      const int* endInds);
                        
- int SgBoxIterator_del(SgBoxIterator_type** self);
+int SgBoxIterator_del(SgBoxIterator_type** self);
  
- int SgBoxIterator_getNumberOfElements(SgBoxIterator_type** self,
-                                       int* num);
+int SgBoxIterator_getNumberOfElements(SgBoxIterator_type** self,
+                                      int* num);
  
- int SgBoxIterator_getElement(SgBoxIterator_type** self,
-                              int index,
-                              int inds[]);
+int SgBoxIterator_getElement(SgBoxIterator_type** self,
+                             int index,
+                             int inds[]);
 
 #ifdef __cplusplus
 }
