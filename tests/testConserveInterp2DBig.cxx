df3918e370350c708a9fba98b34258f6655bdeb6
Alexander Pletzer <alexander@gokliya.net>
2016-11-22 14:21:52 +1300

    + larger conservative interpolation test

---
+++ tests/testConserveInterp2DBig.cxx
@@ -0,0 +1,113 @@
+/**
+ * Testing cconservative interpolation in 2D
+ */
+
+ #include <vector>
+ #include <cassert>
+ #include <cstdio>
+ #include <iostream>
+ #include "SgConserveInterp2D.h"
+
+/**
+ * Create rectangular grid
+ * @param nodeDims number of nodes in the two directions
+ * @param xmins low corner point of the grid
+ * @param xmaxs high corner point of the grid
+ * @param coords coordinates (output)
+ */
+void createRectangularGrid(const int nodeDims[], 
+                           const double xmins[],
+                           const double xmaxs[], 
+                           double** coords) {
+
+    double deltas[] = {(xmaxs[0] - xmins[0])/double(nodeDims[0] - 1),
+                       (xmaxs[1] - xmins[1])/double(nodeDims[1] - 1)};
+    size_t index = 0;
+    for (size_t i = 0; i < nodeDims[0]; ++i) {
+        for (size_t j = 0; j < nodeDims[1]; ++j) {
+            coords[0][index] = xmins[0] + deltas[0]*i;
+            coords[1][index] = xmins[1] + deltas[1]*j;
+            index++;
+        }
+    }
+}
+
+/**
+ * Create polar grid
+ * @param nodeDims number of nodes in the two directions
+ * @param center center of the polar grid
+ * @param radius radius
+ * @param coords coordinates (output)
+ */
+void createPolarGrid(const int nodeDims[],
+                     const double center[], 
+                     double radius,
+                     double** coords) {
+
+    double deltas[] = {radius / double(nodeDims[0] - 1),
+                       2 * M_PI / double(nodeDims[1] - 1)};
+    size_t index = 0;
+    for (size_t i = 0; i < nodeDims[0]; ++i) {
+        for (size_t j = 0; j < nodeDims[1]; ++j) {
+            double rho = 0.0 + deltas[0]*i;
+            double the = 0.0 + deltas[1]*j;
+            coords[0][index] = rho*cos(the);
+            coords[1][index] = rho*sin(the);
+            index++;
+        }
+    }
+}
+
+bool testPolar() {
+
+    // source grid
+    const int srcDims[] = {101, 201}; // number of nodes
+    const double srcXmins[] = {-1., -1.};
+    const double srcXmaxs[] = {1., 1.};
+    int srcNumPoints = srcDims[0] * srcDims[1];
+    double* srcCoords[] = {new double[srcNumPoints], new double[srcNumPoints]};
+    createRectangularGrid(srcDims, srcXmins, srcXmaxs, srcCoords);
+
+    // destination grid
+    const int dstDims[] = {11, 41};
+    const double center[] = {0., 0.};
+    const double radius = 1.0;
+    int dstNumPoints = dstDims[0] * dstDims[1];
+    double* dstCoords[] = {new double[dstNumPoints], new double[dstNumPoints]};
+    createPolarGrid(dstDims, center, radius, dstCoords);    
+
+    SgConserveInterp2D_type* interp = NULL;
+    SgConserveInterp2D_new(&interp);
+    SgConserveInterp2D_setSrcGrid(&interp, srcDims, (const double**) srcCoords);
+    SgConserveInterp2D_setDstGrid(&interp, dstDims, (const double**) dstCoords);
+    SgConserveInterp2D_computeWeights(&interp);
+    int srcIndex, dstIndex;
+    double weight;
+    double totalWeight = 0;
+    int end = 0;
+    SgConserveInterp2D_reset(&interp);
+    while (!end) {
+        SgConserveInterp2D_get(&interp, &srcIndex, &dstIndex, &weight);
+        totalWeight += weight;
+        end = SgConserveInterp2D_next(&interp);
+    }
+    SgConserveInterp2D_del(&interp);
+
+    std::cout << "testPolar: total weight = " << totalWeight << '\n';
+    int dstNumCells = (dstDims[0] - 1) * (dstDims[1] - 1);
+    if (fabs(totalWeight -  1.0 * dstNumCells) > 1.e-10) {
+        // sum of the weights should match number of dst cells
+        // that fall within the src domain
+        return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char** argv) {
+
+    if (!testPolar()) return 1;
+
+    std::cout << "SUCCESS\n";
+    return 0;
+}
