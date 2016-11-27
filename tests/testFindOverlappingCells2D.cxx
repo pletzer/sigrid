35b0e40e4f78f317e33950ba515c8de38fa16a33
Alex Pletzer <alexander@gokliya.net>
2016-11-27 13:09:11 +1300

    consolidating the grid creation

--- tests/testFindOverlappingCells2D.cxx
+++ tests/testFindOverlappingCells2D.cxx
@@ -2,59 +2,12 @@
  * Testing cconservative interpolation in 2D
  */
 
- #include <vector>
- #include <cassert>
- #include <cstdio>
- #include <iostream>
- #include "SgFindOverlappingCells2D.h"
-
-/**
- * Create rectangular grid
- * @param nodeDims number of nodes in the two directions
- * @param xmins low corner point of the grid
- * @param xmaxs high corner point of the grid
- * @param coords coordinates (output)
- */
-void createRectangularGrid(const int nodeDims[], 
-                           const double xmins[],
-                           const double xmaxs[], 
-                           double** coords) {
-
-    double deltas[] = {(xmaxs[0] - xmins[0])/double(nodeDims[0] - 1),
-                       (xmaxs[1] - xmins[1])/double(nodeDims[1] - 1)};
-    size_t index = 0;
-    for (size_t i = 0; i < nodeDims[0]; ++i) {
-        for (size_t j = 0; j < nodeDims[1]; ++j) {
-            coords[0][index] = xmins[0] + deltas[0]*i;
-            coords[1][index] = xmins[1] + deltas[1]*j;
-            index++;
-        }
-    }
-}
-
-/**
- * Create polar grid
- * @param nodeDims number of nodes in the two directions
- * @param radius radius
- * @param coords coordinates (output)
- */
-void createPolarGrid(const int nodeDims[], 
-                     double radius,
-                     double** coords) {
-
-    double deltas[] = {radius / double(nodeDims[0] - 1),
-                       2 * M_PI / double(nodeDims[1] - 1)};
-    size_t index = 0;
-    for (size_t i = 0; i < nodeDims[0]; ++i) {
-        for (size_t j = 0; j < nodeDims[1]; ++j) {
-            double rho = 0.0 + deltas[0]*i;
-            double the = 0.0 + deltas[1]*j;
-            coords[0][index] = rho*cos(the);
-            coords[1][index] = rho*sin(the);
-            index++;
-        }
-    }
-}
+#include <vector>
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include "SgFindOverlappingCells2D.h"
+#include "createGrids2D.h"
 
 bool testRect() {
 
