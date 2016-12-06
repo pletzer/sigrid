38964bc97e7b98fd4f12e0b37c2b8fa49157f5d6
Alexander Pletzer <alexander@gokliya.net>
2016-12-06 16:13:05 +1300

    nicer printing

--- tests/testFindPointInCellPolar.cxx
+++ tests/testFindPointInCellPolar.cxx
@@ -12,110 +12,119 @@
 #include "CmdLineArgParser.h"
 
 std::vector<double> getDoubleVectorFromString(const std::string& s) {
-	std::vector<double> res;
-	size_t begPos = 0;
-	bool cont = true;
-	while (cont) {
-		size_t endPos = s.find(',', begPos);
-		size_t n = endPos - begPos;
-		std::string sval = s.substr(begPos, n);
-		double val = atof(sval.c_str());
-		res.push_back(val);
-		begPos = endPos + 1;
-		if (endPos > s.size()) {
-			cont = false;
-		}
-	}
-	return res;
+    std::vector<double> res;
+    size_t begPos = 0;
+    bool cont = true;
+    while (cont) {
+        size_t endPos = s.find(',', begPos);
+        size_t n = endPos - begPos;
+        std::string sval = s.substr(begPos, n);
+        double val = atof(sval.c_str());
+        res.push_back(val);
+        begPos = endPos + 1;
+        if (endPos > s.size()) {
+            cont = false;
+        }
+    }
+    return res;
 }
 
 int main(int argc, char** argv) {
 
-	CmdLineArgParser prsr;
-	prsr.set("-p", std::string("0., 0."), "Target position");
-	prsr.set("-i", std::string("0., 0."), "Initial index location (guess)");
-	prsr.set("-nr", 11, "Number of grid radii");
-	prsr.set("-nt", 33, "Number of grid poloidal points");
-	prsr.set("-m", 100, "Max number of iterations");
-	prsr.set("-t", 1.e-10, "Tolerance in physical space");
-	prsr.parse(argc, argv);
+    CmdLineArgParser prsr;
+    prsr.set("-p", std::string("0., 0."), "Target position");
+    prsr.set("-i", std::string("0., 0."), "Initial index location (guess)");
+    prsr.set("-nr", 11, "Number of grid radii");
+    prsr.set("-nt", 33, "Number of grid poloidal points");
+    prsr.set("-m", 100, "Max number of iterations");
+    prsr.set("-t", 1.e-10, "Tolerance in physical space");
+    prsr.parse(argc, argv);
 
-	int nitermax = prsr.get<int>("-m");
-	double tolpos = prsr.get<double>("-t");
-	int nt = prsr.get<int>("-nt");
-	int nr = prsr.get<int>("-nr");
+    int nitermax = prsr.get<int>("-m");
+    double tolpos = prsr.get<double>("-t");
+    int nt = prsr.get<int>("-nt");
+    int nr = prsr.get<int>("-nr");
 
-	std::vector<double> targetPoint = getDoubleVectorFromString(prsr.get<std::string>("-p"));
-	std::vector<double> dIndices = getDoubleVectorFromString(prsr.get<std::string>("-i"));
+    std::vector<double> targetPoint = getDoubleVectorFromString(prsr.get<std::string>("-p"));
+    std::vector<double> dIndices = getDoubleVectorFromString(prsr.get<std::string>("-i"));
 
-	int ier;
-	// number of dimensions
-	const int ndims = 2;
+    int ier;
+    // number of dimensions
+    const int ndims = 2;
 
-	std::cout << "target point is ";
-	for (size_t i = 0; i < ndims; ++i) std::cout << targetPoint[i] << ' ';
-	std::cout << '\n';
+    std::cout << "target point is ";
+    for (size_t i = 0; i < ndims; ++i) std::cout << targetPoint[i] << ' ';
+    std::cout << '\n';
 
-	// grid
-	
-	int dims[] = {nr, nt};
-	double* coords[] = {new double[nr * nt], new double[nr * nt]};
-	const double center[] = {0., 0.};
-	const double radius = 1.0;
-	createPolarGrid(dims, center, radius, coords);
+    // grid
+    
+    int dims[] = {nr, nt};
+    double* coords[] = {new double[nr * nt], new double[nr * nt]};
+    const double center[] = {0., 0.};
+    const double radius = 1.0;
+    createPolarGrid(dims, center, radius, coords);
 
-	double pos[ndims];
-	double oldPos[ndims];
+    double pos[ndims];
+    double oldPos[ndims];
 
-	SgFindPointInCell_type* picf = NULL;
-	ier = SgFindPointInCell_new(&picf, nitermax, tolpos);
-	assert(ier == 0);
+    SgFindPointInCell_type* picf = NULL;
+    ier = SgFindPointInCell_new(&picf, nitermax, tolpos);
+    assert(ier == 0);
 
-	// periodic in theta
-	const int periodicity[] = {0, 1};
-	ier = SgFindPointInCell_setGrid(&picf, ndims, dims, periodicity,
-		                            (const double**) &coords[0]);
-	assert(ier == 0);
+    // periodic in theta
+    const int periodicity[] = {0, 1};
+    ier = SgFindPointInCell_setGrid(&picf, ndims, dims, periodicity,
+                                    (const double**) &coords[0]);
+    assert(ier == 0);
 
-	ier = SgFindPointInCell_reset(&picf, &dIndices[0], &targetPoint[0]);
-	assert(ier == 0);
+    ier = SgFindPointInCell_reset(&picf, &dIndices[0], &targetPoint[0]);
+    assert(ier == 0);
 
-	int end = 0;
-	size_t icount = 0;
-	double error = std::numeric_limits<double>::max();
-	while (end == 0) {
+    int end = 0;
+    size_t icount = 0;
+    double error = std::numeric_limits<double>::max();
 
-		std::cout << "Iteration: " << icount << '\n';
+    printf("iter    old_position               new_position             error\n");
+    while (end == 0) {
 
-		ier = SgFindPointInCell_getPosition(&picf, oldPos);
-		assert(ier == 0);
+        printf("%d   ", icount);
 
-		end = SgFindPointInCell_next(&picf);
+        ier = SgFindPointInCell_getPosition(&picf, oldPos);
+        assert(ier == 0);
 
-		ier = SgFindPointInCell_getPosition(&picf, pos);
-		assert(ier == 0);
+        for (size_t i = 0; i < ndims; ++i) {
+            printf("%10.4f ", oldPos[i]);
+        }
+        printf("    ");
 
-		std::cout << "iter " << icount << " position old = ";
-		for (int j = 0; j < ndims; ++j) std::cout << oldPos[j] << " ";
-		std::cout << " -> new = ";
-	    for (int j = 0; j < ndims; ++j) std::cout << pos[j] << " ";
 
-		ier = SgFindPointInCell_getError(&picf, &error);
-		assert(ier == 0);
-		std::cout << " (error = " << error << ")\n";
+        end = SgFindPointInCell_next(&picf);
 
-		icount++;
-	}
+        ier = SgFindPointInCell_getPosition(&picf, pos);
+        assert(ier == 0);
 
-	ier = SgFindPointInCell_del(&picf);
-	assert(ier == 0);
+        for (size_t i = 0; i < ndims; ++i) {
+            printf("%10.3f ", pos[i]);
+        }
+        printf("    ");
 
-	if (error > tolpos) {
-		// did not converge
-		std::cout << "ERROR " << end << '\n';
-		return 1;
-	}
+        ier = SgFindPointInCell_getError(&picf, &error);
+        assert(ier == 0);
 
-	std::cout << "SUCCESS\n";
-	return 0;
+        printf("    %e\n", error);
+
+        icount++;
+    }
+
+    ier = SgFindPointInCell_del(&picf);
+    assert(ier == 0);
+
+    if (error > tolpos) {
+        // did not converge
+        std::cout << "ERROR " << end << '\n';
+        return 1;
+    }
+
+    std::cout << "SUCCESS\n";
+    return 0;
 }
