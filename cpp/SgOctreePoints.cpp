3df16efedd2c9980d0bc6d8cebcc214484c970d5
Alex Pletzer <alexander@gokliya.net>
2017-01-23 09:04:33 +1300

    ~ now compiles

--- cpp/SgOctreePoints.cpp
+++ cpp/SgOctreePoints.cpp
@@ -7,9 +7,10 @@
 #include <iostream>
 
 extern "C"
-int SgOctreePoints_new(SgOctreePoints_type** self,
-                          int nitermax, double tolpos) {
-    *self = new SgOctreePoints_type(nitermax, tolpos);
+int SgOctreePoints_new(SgOctreePoints_type** self, 
+                       int numLevels, int ndims, int npoints, const double* points) {
+    std::vector<double> pts(points, points + npoints*ndims);
+    *self = new SgOctreePoints_type((size_t) numLevels, (size_t) ndims, pts);
     return 0;
 }
       
@@ -17,60 +18,4 @@ extern "C"
 int SgOctreePoints_del(SgOctreePoints_type** self) {
     delete *self;
     return 0;
-}
-
-extern "C"
-int SgOctreePoints_setGrid(SgOctreePoints_type** self, 
-                              int ndims, const int dims[],
-                              const int periodicity[], 
-                              const double** coords) {
-    (*self)->setGrid(ndims, dims, periodicity, coords);
-    return 0;
-}
-
-extern "C"
-int SgOctreePoints_getPosition(SgOctreePoints_type** self,
-                                  double pos[]) {
-    std::vector<double> position = (*self)->getPosition();
-    for (size_t i = 0; i < position.size(); ++i) {
-        pos[i] = position[i];
-    }
-    return 0;
-}
-
-extern "C"
-int SgOctreePoints_getError(SgOctreePoints_type** self,
-                               double* error) {
-    *error = (*self)->getError();
-    return 0;
-}
-
-extern "C"
-int SgOctreePoints_reset(SgOctreePoints_type** self, 
-                            const double dIndices[],
-                            const double targetPoint[]) {
-    (*self)->reset(dIndices, targetPoint);
-    return 0;
-}
-
-extern "C"
-int SgOctreePoints_next(SgOctreePoints_type** self) {
-    return (*self)->next();
-}
-
-extern "C"
-int SgOctreePoints_getIndices(SgOctreePoints_type** self,
-                                 double dIndices[]) {
-    std::vector<double> dInds = (*self)->getIndices();
-    for (size_t i = 0; i < dInds.size(); ++i) {
-        dIndices[i] = dInds[i];
-    }
-    return 0;
-}
-
-extern "C"
-int SgOctreePoints_getErrorHistory(SgOctreePoints_type** self,
-                                      int* niter, double** errorHistory) {
-  (*self)->getErrorHistory(niter, errorHistory);
-  return 0;
 }
--- cpp/SgOctreePoints.h
+++ cpp/SgOctreePoints.h
@@ -13,11 +13,11 @@
 #include <vector>
 
 // min/max coordinates of box
-struct SgOctreElement {
+struct SgOctreeElement {
     std::vector<double> xmins;
     std::vector<double> xmaxs;
-    SgOctreElement(xLo, xHi) : xmins(xLo), xmaxs(xHi) {
-    }
+    SgOctreeElement(const std::vector<double>& xLo, const std::vector<double>& xHi) : xmins(xLo), xmaxs(xHi) {
+    }  
 
 };
 
@@ -25,13 +25,17 @@ struct SgOctreePoints_type {
 
     // maps a key to a box in space. A key is a list of ints, each
     // int being a flat index of a box
-    std::map< std::vector<size_t>, SgOctreElement > octree;
+    std::map< std::vector<size_t>, SgOctreeElement > octree;
 
     // maps a node index to a key
     std::map< size_t, std::vector<size_t> > node2Key;
 
     // need this to convert flat box indices to index sets
     std::vector<size_t> prodDims;
+
+    // domain sizes
+    std::vector<double> xmins;
+    std::vector<double> xmaxs;
 
     // number of space dimensions
     size_t ndims;
@@ -52,8 +56,8 @@ SgOctreePoints_type(size_t numLevels, size_t ndims, const std::vector<double>& p
     for (size_t i = 0; i < points.size(); ++i) {
         size_t dim = i % ndims;
         double p = points[i];
-        xmins[dim] = (p < xmins[dim]? p: xmins[dims]);
-        xmaxs[dim] = (p > xmaxs[dim]? p: xmaxs[dims]);
+        xmins[dim] = (p < xmins[dim]? p: xmins[dim]);
+        xmaxs[dim] = (p > xmaxs[dim]? p: xmaxs[dim]);
     }
 
     // used to map the flat indices to index sets
@@ -73,8 +77,8 @@ SgOctreePoints_type(size_t numLevels, size_t ndims, const std::vector<double>& p
             // compute the box lo/hi corners in (0, 1) space
             std::vector<double> xLo(ndims, 0);
             std::vector<double> xHi(ndims, 0);
-            double dx = 1.0 / std::pow(2, level2);
             for (size_t level2 = 0; level2 < numLevels; ++level2) {
+                double dx = 1.0 / std::pow(2, level2);
                 for (size_t j = 0; j < ndims; ++j) {
                     // index of the index set
                     size_t boxIndx = (key[level2] / this->prodDims[j] % 2);
@@ -91,21 +95,22 @@ SgOctreePoints_type(size_t numLevels, size_t ndims, const std::vector<double>& p
             }
 
             // add entry to the tree
-            SgOctreElement elem(lo, hi);
-            this->octree.insert(key, elem);
-            }
+            SgOctreeElement elem(xLo, xHi);
+            this->octree.insert(std::pair< std::vector<size_t>, SgOctreeElement>(key, elem));
         }
     }
 
     // attach each point to a sub-box
     size_t numPoints = points.size() / ndims;
     for (size_t i = 0; i < numPoints; ++i) {
-        std::vector<size_t> key = this->getKey(&points[i*ndims]);
+        std::vector<size_t> key = this->getKey(&points[i*ndims], this->nlevs);
         this->node2Key.insert(std::pair<size_t, std::vector<size_t> >::pair(i, key));
     }
 
     this->ndims = ndims;
     this->nlevs = numLevels;
+    this->xmins = xmins;
+    this->xmaxs = xmaxs;
 }
 
 /**
@@ -115,25 +120,31 @@ SgOctreePoints_type(size_t numLevels, size_t ndims, const std::vector<double>& p
 }
 
 
-const std::vector<double>& getLo(const std::vector<size_t>& key) {
-    return this->octree[key].xmins;
+const std::vector<double>& getLo(const std::vector<size_t>& key) const {
+    std::map< std::vector<size_t>, SgOctreeElement >::const_iterator
+        it = this->octree.find(key);
+    if (it != this->octree.end()) {
+        return it->second.xmins;
+    }
+    return this->xmins;
 }
 
-const std::vector<double>& getHi(const std::vector<size_t>& key) {
-    return this->octree[key].xmaxs;
-}
-
-const std::vector<size_t>& getPointIndices(const std::vector<size_t>& key) {
-    return this->octree[key].indices;
+const std::vector<double>& getHi(const std::vector<size_t>& key) const {
+    std::map< std::vector<size_t>, SgOctreeElement >::const_iterator
+        it = this->octree.find(key);
+    if (it != this->octree.end()) {
+        return it->second.xmaxs;
+    }
+    return this->xmaxs;
 }
 
 private:
 
-    std::vector<size_t> getKey(const double* pt, size_t lev = this->nlevs) {
+    std::vector<size_t> getKey(const double* pt, size_t lev) {
         // normalize
-        std::vector<double> x(point, point + this->ndims);
+        std::vector<double> x(pt, pt + this->ndims);
         for (size_t j = 0; j < this->ndims; ++j) {
-            double x[j] = (pt[j] - this->xmins[i])/(this->xmaxs[j] - this->xmins[j]); // should not be zero
+            x[j] = (pt[j] - this->xmins[j])/(this->xmaxs[j] - this->xmins[j]); // should not be zero
         }
         std::vector<size_t> key(lev, 0);
         for (size_t el = 0; el < lev; ++el) {
