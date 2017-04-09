df8cfe0ad99a7b38e8d33ab4ea261b172cc7a05e
Alex Pletzer <alexander@gokliya.net>
2017-04-09 20:21:48 +1200

    now running

--- examples/sgutils.py
+++ examples/sgutils.py
@@ -4,5 +4,5 @@ def createRectilinearGrid(dims, xmin, xmax):
 	x0 = numpy.linspace(xmin[0], xmax[0], dims[0])
 	x1 = numpy.linspace(xmin[1], xmax[1], dims[1])
 	xx0 = numpy.outer(x0, numpy.ones((dims[1],), numpy.float64))
-	xx1 = numpy.outer(numpy.ones((dims[1],), numpy.float64), x1)
+	xx1 = numpy.outer(numpy.ones((dims[0],), numpy.float64), x1)
 	return (xx0, xx1)
