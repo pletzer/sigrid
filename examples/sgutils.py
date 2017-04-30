85ce2622ede5b41b0c9702a7ed7022dd277fd1c2
Alex Pletzer <alexander@gokliya.net>
2017-04-30 17:03:07 +1200

    work in progress

--- examples/sgutils.py
+++ examples/sgutils.py
@@ -8,6 +8,13 @@ def createRectilinearGrid(dims, xmin, xmax):
 	xx1 = numpy.outer(numpy.ones((dims[0],), numpy.float64), x1)
 	return (xx0, xx1)
 
+def createPolarGrid(dims, radius):
+    x0 = numpy.linspace(0.0, radius, dims[0])     # rho
+    x1 = numpy.linspace(0.0, 2*numpy.pi, dims[1]) # theta
+    xx0 = numpy.outer(x0, numpy.ones((dims[1],), numpy.float64))
+    xx1 = numpy.outer(numpy.ones((dims[0],), numpy.float64), x1)
+    return (xx0, xx1)
+
 def saveLineVtk(filename, coords, data):
 
     numPoints = len(coords[0])
@@ -34,7 +41,44 @@ def saveLineVtk(filename, coords, data):
         f.write("{} ".format(data[i]))
     f.write("\n")
 
-    f.close()	
+    f.close()
+
+def saveFlowVtk(filename, coords, flux0, flux1):
+
+    dims = coords[0].shape
+
+    f = open(filename, 'w')
+
+    f.write("# vtk DataFile Version 2.0\n")
+    f.write("flow\n")
+    f.write("ASCII\n")
+    f.write("DATASET STRUCTURED_GRID\n")
+    f.write("DIMENSIONS 1 {} {}\n".format(dims[1], dims[0])) # inverse order
+    numPoints = dims[0] * dims[1]
+    f.write("POINTS {} float\n".format(numPoints))
+    for i in range(dims[0]):
+        for j in range(dims[1]):
+            f.write("{} {} 0.0\n".format(coords[0][i, j], coords[1][i, j]))
+ 
+    numCells = (dims[0] - 1) * (dims[1] - 1)
+
+    f.write("CELL_DATA {}\n".format(numCells))
+    f.write("SCALARS velocity float 3\n")
+    f.write("LOOKUP_TABLE default\n")
+
+    vx = flux1/(coords[1][:, 1:] - coords[1][:, :-1])
+    vy = flux0/(coords[0][1:, :] - coords[0][:-1, :])
+
+    # average the velocity field to cell centres
+    vxAvg = 0.5*(vx[:-1, :] + vx[1:, :])
+    vyAvg = 0.5*(vy[:, :-1] + vy[:, 1:])
+
+    for i in range(dims[0] - 1):
+        for j in range(dims[1] - 1):
+            f.write("{} {} 0.0\n".format(vxAvg[i, j], vyAvg[i, j]))
+
+    f.close();
+
 
 def saveStreamlinesVtk(filename, coords, psi):
 
