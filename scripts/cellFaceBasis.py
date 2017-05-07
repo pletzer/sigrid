bf9c045fc3cce740990a38e90f87fadaf0872212
Alex Pletzer <alexander@gokliya.net>
2017-05-07 21:02:34 +1200

    code cleaning

--- scripts/cellFaceBasis.py
+++ scripts/cellFaceBasis.py
@@ -13,9 +13,9 @@ class FaceBasis:
         # build the mesh
         dXi = 1.0 / float(dims[0] - 1)
         dEta = 1.0 / float(dims[1] - 1)
-        xsi1d = numpy.linspace(0., 1., dims[0])
+        xi1d = numpy.linspace(0., 1., dims[0])
         eta1d = numpy.linspace(0., 1., dims[1])
-        xi = numpy.outer(xsi1d, numpy.ones(dims[1], numpy.float64))
+        xi = numpy.outer(xi1d, numpy.ones(dims[1], numpy.float64))
         eta = numpy.outer(numpy.ones(dims[0], numpy.float64), eta1d)
 
         # apply coordinate transformations
@@ -23,8 +23,8 @@ class FaceBasis:
         y = eval(yExpr)
 
         # compute averages
-        x1Avg = 0.5*(x[:-1, :] + x[1:, :])
-        x2Avg = 0.5*(x[:, :-1] + x[:, 1:])
+        x1Avg = 0.5*(x[:-1, :] + x[1:, :]) # x average in the first coordinate
+        x2Avg = 0.5*(x[:, :-1] + x[:, 1:]) # x average in the second coordinate
         y1Avg = 0.5*(y[:-1, :] + y[:1, :])
         y2Avg = 0.5*(y[:, :-1] + y[:, 1:])
 
@@ -42,10 +42,11 @@ class FaceBasis:
         jac = dxdxi * dydeta - dydxi * dxdeta
 
         # basis functions
-        self.bases = {(-1, 0): [(1 - eta)*dxdeta/jac, (1 - eta)*dydeta/jac],
-                      (-1, 1): [(0 + eta)*dxdeta/jac, (0 + eta)*dydeta/jac],
-                      (0, -1): [(1 - xi)*dxdxi/jac, (1 - xi)*dydxi/jac],
-                      (1, -1): [(0 + xi)*dxdxi/jac, (0 + xi)*dydxi/jac]}
+        self.bases = {(-1, 0): [(1 - eta)*dxdeta/jac, (1 - eta)*dydeta/jac], # ok
+                      (-1, 1): [(0 + eta)*dxdeta/jac, (0 + eta)*dydeta/jac], # ok
+                      (0, -1): [(1 - xi)*dxdxi/jac, (1 - xi)*dydxi/jac], # looks wrong
+                      (1, -1): [(0 + xi)*dxdxi/jac, (0 + xi)*dydxi/jac], # looks wrong
+                      }
 
         self.x = x
         self.y = y
@@ -72,13 +73,14 @@ class FaceBasis:
         f.write("CELL_DATA {}\n".format(numCells))
 
         # iterate over the basis functions
-        for k, b in self.bases.items():
-            basisName = '_{}_{}'.format(k[0], k[1])
+        for side, basis in self.bases.items():
+            basisName = '_{}_{}'.format(side[0], side[1])
             f.write("SCALARS {} float 3\n".format(basisName))
             f.write("LOOKUP_TABLE default\n")
+            vx, vy = basis[0], basis[1]
             for i in range(dims[0] - 1):
                 for j in range(dims[1] - 1):
-                    f.write("{} {} 0.0".format(b[0][i, j], b[1][i, j]))
+                    f.write("{} {} 0.0".format(vx[i, j], vy[i, j]))
 
         f.close()
 
@@ -89,9 +91,9 @@ def main():
     parser = argparse.ArgumentParser(description='Generate VTK plots of the face basis functions in 2D.')
     parser.add_argument('--dims', dest='dims', default='11,11',
                     help='Number of nodes in xi and eta')
-    parser.add_argument('--xExpr', dest='xExpr', default='xi',
+    parser.add_argument('--xExpr', dest='xExpr', default='xi*cos(eta) - 0.2*eta',
                     help='X coordinate transformation as a function of xi and eta')
-    parser.add_argument('--yExpr', dest='yExpr', default='eta',
+    parser.add_argument('--yExpr', dest='yExpr', default='eta/2.0 + 0.4*sin(0.3*pi*xi*eta)',
                     help='Y coordinate transformation as a function of xi and eta')
     parser.add_argument('--filename', dest='filename', default='cellFaceBasis.vtk',
                     help='Filename for storing the cell centred vector field of each basis function')
